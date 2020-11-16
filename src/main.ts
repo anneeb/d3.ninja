b42de725d72d7bb5370a7060325661d939840300
anneeb <ahuang852@gmail.com>
2020-11-15 19:56:53 -0500

    add env

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "app/app.module";
-import { isProduction } from "utils/is-production";
+import { environment } from "environments/environment";
 
-if (isProduction) {
+if (environment.production) {
   enableProdMode();
 }
 
